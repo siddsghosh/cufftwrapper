0d318edda6772e9f909228d1efe03eaf08c80c37
Sidd Ghosh <siddsghosh@gmail.com>
2021-06-03 12:10:08 -0600

    Some bug fixes, now it is pretty much working, though some more tests needed.

--- fortranfft.f90
+++ fortranfft.f90
@@ -16,7 +16,7 @@ contains
    allocate(a2d_c(2,ny,deljx,deliz))
    a2d_c = 0.
    init_state = 0
-!!$acc enter data create(a2d_c)
+!$acc enter data copyin(a2d_c)
    end subroutine alloc_a2d_c
 end module scratch_data
 
@@ -25,8 +25,8 @@ subroutine fortcud2z( nx, iys, iye, iz1, iz2, fn, ax )
 implicit none
 integer :: nx, iys, iye, iz1, iz2, iz, iy, ix
 real(8) :: ax(nx+2,iys:iye,iz1:iz2), fn
-!!$acc data copy(ax), copyin(fn)
-!!$acc kernels !collapse(3)
+!$acc data copy(ax), copyin(fn)
+!$acc parallel loop collapse(3)
 do iz=iz1,iz2
    do iy=iys,iye
       do ix=1,nx
@@ -35,9 +35,7 @@ do iz=iz1,iz2
       ax(nx+1,iy,iz) = 0.; ax(nx+2,iy,iz) = 0.
    enddo
 enddo
-!!$acc end kernels
-!!$acc end data
-!$acc data copy(ax)
+!$acc end parallel
 !$acc host_data use_device(ax)
 call cud2z(nx, (iye-iys+1), (iz2-iz1+1), ax)
 !$acc end host_data
@@ -63,38 +61,33 @@ implicit none
 integer :: ny, nx, nz, jxs, jxe, iz1, iz2, iz, ix, iy, ix1
 real(8) :: at(ny,jxs:jxe,iz1:iz2)
 if (init_state.eq.1) call alloc_a2d_c( ny, jxs, jxe, iz1, iz2 )
-!!$acc data copy(at)
-!!$acc kernels !collapse(3)
+!$acc data copy(at) present(a2d_c)
+!$acc parallel loop collapse(3)
 do iz=iz1,iz2
-   ix1 = 1
    do ix=jxs,jxe,2
       do iy=1,ny
+         ix1 = (ix-jxs)/2 + 1
          a2d_c(1,iy,ix1,iz) = at(iy,ix,iz)
          a2d_c(2,iy,ix1,iz) = at(iy,ix+1,iz)
       enddo
-      ix1 = ix1 + 1
    enddo
 enddo
-!!$acc end kernels
-!!$acc end data
-!$acc data copy(a2d_c)
+!$acc end parallel
 !$acc host_data use_device(a2d_c)
 call cuz2zf(ny, deljx, deliz, a2d_c)
 !$acc end host_data
-!$acc end data
-!!$acc kernels !collapse(3)
+!$acc parallel loop collapse(3)
 do iz=iz1,iz2
-   ix1 = 1
    do ix=jxs,jxe,2
       do iy=1,ny
+         ix1 = (ix-jxs)/2 + 1
          at(iy,ix,iz)   = a2d_c(1,iy,ix1,iz)
          at(iy,ix+1,iz) = a2d_c(2,iy,ix1,iz)
       enddo
-      ix1 = ix1 + 1
    enddo
 enddo
-!!$acc end kernels
-!!$acc end data
+!$acc end parallel
+!$acc end data
 end subroutine fortcuz2zf
 
 
@@ -104,36 +97,31 @@ implicit none
 integer :: ny, jxs, jxe, ix1, iz1, iz2, iz, ix, iy
 real(8) :: at(ny,jxs:jxe,iz1:iz2)
 if (init_state.eq.1) call alloc_a2d_c( ny, jxs, jxe, iz1, iz2 )
-!!$acc data copy(at)
-!!$acc kernels !collapse(3)
+!$acc data copy(at) present(a2d_c)
+!$acc parallel loop collapse(3)
 do iz=iz1,iz2
-   ix1 = 1
    do ix=jxs,jxe,2
       do iy=1,ny
+         ix1 = (ix-jxs)/2 + 1
          a2d_c(1,iy,ix1,iz) = at(iy,ix,iz)
          a2d_c(2,iy,ix1,iz) = at(iy,ix+1,iz)
       enddo
-      ix1 = ix1 + 1
    enddo
 enddo
-!!$acc end kernels
-!!$acc end data
-!$acc data copy(a2d_c)
+!$acc end parallel
 !$acc host_data use_device(a2d_c)
 call cuz2zb(ny, deljx, deliz, a2d_c)
 !$acc end host_data
-!$acc end data
-!!$acc kernels !collapse(3)
+!$acc parallel loop collapse(3)
 do iz=iz1,iz2
-   ix1 = 1
    do ix=jxs,jxe,2
       do iy=1,ny
+         ix1 = (ix-jxs)/2 + 1
          at(iy,ix,iz)   = a2d_c(1,iy,ix1,iz)
          at(iy,ix+1,iz) = a2d_c(2,iy,ix1,iz)
       enddo
-      ix1 = ix1 + 1
    enddo
 enddo
-!!$acc end kernels
-!!$acc end data
+!$acc end parallel
+!$acc end data
 end subroutine fortcuz2zb
